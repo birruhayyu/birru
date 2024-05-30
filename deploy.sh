af5f3387f935bc2477c38a5f0e7169f187c37202
Birru Shobriaturrahmah <birru.shobriaturrahmah@ieneo.com>
2024-05-30 12:16:18 +0200

    modify

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 
+# abort on errors
+set -e
+
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
@@ -12,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/birruhayyu/cv main
+git push -f git@github.com:birruhayyu/cv.git main:gh-pages
 
 cd -
