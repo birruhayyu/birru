6b89c5a1caf6abb58af161cd7114f378b900abf9
Birru Shobriaturrahmah <birru.shobriaturrahmah@ieneo.com>
2024-05-30 10:55:03 +0200

    change script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:birruhayyu/cv.git gh-pages
+git push -f git@github.com:birruhayyu/cv.git main
 
 cd -
